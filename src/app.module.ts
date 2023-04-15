dea3f3d1d884340f40250b8867213784f8ef9aee
Meithal <hurin8888@gmail.com>
2023-04-15 23:01:23 +0200

    nest g resource todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [ProjectsModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
