e3460704c1a4c524f2f2546b2d10d729ebeb416e
Meithal <hurin8888@gmail.com>
2023-04-19 01:33:09 +0200

    firebase login firebase init firebase deploy

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,13 +1,26 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 // Follow this pattern to import other Firebase services
 // import { } from 'firebase/<service>';
 
-// TODO: Replace the following with your app's Firebase project configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  //...
-};
-
+    apiKey: "AIzaSyCVrgA8IrmKNFTx4HIppI2ufX2LrXYd9sY",
+    authDomain: "cookup-afee1.firebaseapp.com",
+    databaseURL: "https://cookup-afee1-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "cookup-afee1",
+    storageBucket: "cookup-afee1.appspot.com",
+    messagingSenderId: "667379681746",
+    appId: "1:667379681746:web:508f8ccc1c34af7057a00b",
+    measurementId: "G-KQXNXZ6745"
+  };
+  
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
@@ -18,3 +31,9 @@ async function getCities(db) {
   const cityList = citySnapshot.docs.map(doc => doc.data());
   return cityList;
 }
+
+const analytics = getAnalytics(app);
+
+
+
+
